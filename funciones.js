79706ec709a43d5e1a641f8306dc0c73f09480a6
Coffe-Coll <juancoll.velez@gmail.com>
2026-03-04 15:35:31 -0500

    Funciones puras

---
+++ funciones.js
@@ -0,0 +1,13 @@
+function Suma(x) {
+    let accion = x + 5;
+    return accion;
+};
+
+console.log(Suma(7));
+
+function Dividicion(x) {
+    let accion = x/5;
+    return accion;
+};
+
+console.log(Dividicion(25));
