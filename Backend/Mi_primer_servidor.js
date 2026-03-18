f93d5d2d68aa02dd6e3ff7a82b5a8bc888f48d29
Coffe-Coll <juancoll.velez@gmail.com>
2026-03-18 16:13:54 -0500

    PROTOCOLOS_HTTP

--- Backend/Mi_primer_servidor.js
+++ Backend/Mi_primer_servidor.js
@@ -9,11 +9,6 @@ app.get(`/json`, (req,res) => {
     res.json({mensaje: `¡Hola, mundo en formato JSON!` });
 });
 
-app.get(`/coll`, (req,res) => {
-    res.json({mensaje:})
-}
-
-
 app.listen(3000, () => {
     console.log(`Servidor escuchando en el puerto 3000`);
 });
